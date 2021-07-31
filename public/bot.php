44163895d728e3b68eabd080b27cfcb13a21602e
MadiTurkmenbay <poyyyg@gmail.com>
2021-07-31 14:54:02 +0600

    sddf

--- public/bot.php
+++ public/bot.php
@@ -1,12 +1,17 @@
 <?php
 $log = file_get_contents('https://api.telegram.org/bot1649773011:AAFMIGsB10-sntTTSub9j78sGckolNwn3nc/sendMessage?chat_id=642295472&parse_mode=html&text=ҚҰТТssssЫҚТАЙМЫН');
+
+
+use App\Models\TelegramUser;
+
+
 $user = new TelegramUser();
 $user->first_name = 'jhf';
 $user->last_name = 'hfyfyh';
 $user->username = 'jhfjv';
 $user->chat_id = '$chatId';
+$user->save();
 
-use App\Models\TelegramUser;
 
 $token = '1649773011:AAFMIGsB10-sntTTSub9j78sGckolNwn3nc';
 $website = "https://api.telegram.org/bot" . $token;
